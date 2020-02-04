a0beee135aec21030e03d424b72e2ff6d70e6586
Pawan16123 <poojary.pawan16@gmail.com>
2020-02-04 15:09:39 +0530

    header responsive done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 	$('#nav-icon2').click(function(){
         $(this).toggleClass('open');
+        $("#nav-icon2 span").toggleClass('halfWidth');
+        $(".mobile-nav").toggleClass('toggledisplay');
        
     });
-    $("#nav-icon2").click(function(){
-        $("span").toggleClass('halfWidth');
-    });
+    // $("#nav-icon2").click(function(){
+    //     $("#nav-icon2 span").toggleClass('halfWidth');
+    // });
+
+    
 });
